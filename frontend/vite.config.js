b022a7a5922eef46405a0acc6db668118ebba546
Nukee25 <kshitijtiwari.india@gmail.com>
2026-03-22 13:02:31 +0530

    Enabled docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const apiTarget = process.env.VITE_API_PROXY_TARGET || 'http://parikshak.duckdns.org/api' || 'http://172.24.0.2:8000'
+const apiTarget = process.env.VITE_API_PROXY_TARGET || 'http://yatharth.duckdns.org/api' || 'http://172.24.0.2:8000'
 
 export default defineConfig({
   plugins: [react()],
@@ -9,7 +9,7 @@ export default defineConfig({
     host: '0.0.0.0', // listen on all interfaces
     port: 5173,      // comma here is essential
     allowedHosts: [
-      'parikshak.duckdns.org'
+      'yatharth.duckdns.org'
     ],
     proxy: {
       '/api': apiTarget
